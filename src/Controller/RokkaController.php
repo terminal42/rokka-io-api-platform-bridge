a1faf12bc1fb0e178a07a556d09e43c49910ba96
Yanick Witschi <yanick.witschi@terminal42.ch>
2019-01-04 10:50:35 +0100

    Added unit test for POST /sourceimages

--- src/Controller/RokkaController.php
+++ src/Controller/RokkaController.php
@@ -17,6 +17,7 @@ use Http\Client\HttpClient;
 use Http\Discovery\HttpClientDiscovery;
 use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
@@ -36,20 +37,14 @@ class RokkaController
     private $bridgeEndpoint;
 
     /**
-     * @var string
-     */
-    private $defaultOrganization;
-
-    /**
      * @var HttpClient
      */
     private $client;
 
-    public function __construct(string $apiKey, string $bridgeEndpoint, string $defaultOrganization = null, HttpClient $client = null)
+    public function __construct(string $apiKey, string $bridgeEndpoint, HttpClient $client = null)
     {
         $this->apiKey = $apiKey;
         $this->bridgeEndpoint = $bridgeEndpoint;
-        $this->defaultOrganization = $defaultOrganization;
         $this->client = $client ?? HttpClientDiscovery::find();
     }
 
@@ -61,22 +56,15 @@ class RokkaController
         $psr7Factory = new DiactorosFactory();
         $httpFoundationFactory = new HttpFoundationFactory();
 
-        // Remove personal headers
-        $request->cookies = new ParameterBag();
-        $request->headers->remove('Authorization');
+        // Extract rokka.io path from attributes
+        $rokkaPath = $this->createRokkaPath($request);
 
-        // Add API key header
-        $request->headers->set('Api-Key', $this->apiKey);
-
-        // Ensure content-length is present
-        if (!$request->headers->has('Content-Length')) {
-            $request->headers->set('Content-Length', \strlen($request->getContent()));
-        }
+        // Normalize request
+        $this->normalizeRequest($request);
 
         $psrRequest = $psr7Factory->createRequest($request);
 
         // Override URI
-        $rokkaPath = $this->createRokkaPath($request);
         $psrRequest = $psrRequest->withUri(new Uri('https://api.rokka.io'.$rokkaPath));
 
         // Send the request
@@ -85,24 +73,68 @@ class RokkaController
         $response = $httpFoundationFactory->createResponse($psrResponse);
 
         // Normalize response
-        $response = $this->normalizeResponse($response, $rokkaPath);
+        $this->normalizeResponse($response, $rokkaPath);
 
         return $response;
+    }
+
+    /**
+     * Normalizes the request in a way it does not expose useless information from the client to the
+     * rokka.io servers (e.g. authentication details) and provides the api key.
+     */
+    private function normalizeRequest(Request $request)
+    {
+        // Remove cookies
+        $request->cookies = new ParameterBag();
+
+        // Only allow whitelisted headers
+        $headerWhitelist = [
+            'User-Agent',
+            'Accept',
+            'Content-Type',
+            'Content-Length',
+        ];
+
+        foreach (array_keys($request->headers->all()) as $header) {
+            if (!\in_array($header, $headerWhitelist, true)) {
+                $request->headers->remove($header);
+            }
+        }
+
+        // Add API key header
+        $request->headers->set('Api-Key', $this->apiKey);
+
+        // Ensure content-length is present
+        if (!$request->headers->has('Content-Length')) {
+            if ('' !== $request->getContent()) {
+                $request->headers->set('Content-Length', \strlen($request->getContent()));
+            } elseif ($request->files->count()) {
+                $length = 0;
+                /** @var UploadedFile $file */
+                foreach ($request->files->all() as $file) {
+                    $length += $file->getSize();
+                }
+                $request->headers->set('Content-Length', $length);
+            } else {
+                $request->headers->set('Content-Length', '0');
+            }
+        }
+
+        // Remove attributes
+        $request->attributes = new ParameterBag();
     }
 
     /**
      * Normalizes the response, meaning it searches for links and automatically prefixes them with the bridge
      * endpoint so for the end user this feels like a natural API endpoint.
      */
-    private function normalizeResponse(Response $response, string $rokkaPath): Response
+    private function normalizeResponse(Response &$response, string $rokkaPath)
     {
         if ('application/json' === $response->headers->get('Content-Type')) {
             $content = json_decode($response->getContent(), true);
             $content = $this->recursiveReplaceLinks($content, $rokkaPath);
             $response = new JsonResponse($content);
         }
-
-        return $response;
     }
 
     private function recursiveReplaceLinks(array $content, string $rokkaPath): array
---
+++ tests/Controller/RokkaControllerTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * terminal42/rokka-io-api-platform-bridge
+ *
+ * @copyright  Copyright (c) 2008-2019, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    MIT
+ * @link       http://github.com/terminal42/rokka-io-api-platform-bridge
+ */
+
+namespace Terminal42\RokkaApiPlatformBridge\Test\Controller;
+
+use Http\Client\HttpClient;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Terminal42\RokkaApiPlatformBridge\Controller\RokkaController;
+use Zend\Diactoros\Response as Psr7Response;
+use Zend\Diactoros\ServerRequest;
+
+class RokkaControllerTest extends TestCase
+{
+    /**
+     * @dataProvider controllerProvider
+     */
+    public function testController(Request $request, callable $expectedRequestValidator, ResponseInterface $mockedResponse, Response $expectedResponse)
+    {
+        $client = $this->createMock(HttpClient::class);
+        $client
+            ->expects($this->once())
+            ->method('sendRequest')
+            ->with($this->callback($expectedRequestValidator))
+            ->willReturn($mockedResponse);
+
+        $controller = new RokkaController('api-key', '/images', $client);
+
+        $this->assertSame(serialize($controller($request, 'foobar-organization')), serialize($expectedResponse)); // csfixer turns assertEquals() into assertSame()
+        //$this->assertEquals($controller($request, 'foobar-organization'), $expectedResponse);
+    }
+
+    public function controllerProvider()
+    {
+        yield 'Test creating a new source image' => [
+            $this->createRequest('/images/sourceimages', '/sourceimages/{organization}', 'foobar-organization', 'POST', [], 'filedata'),
+            function (ServerRequest $request) {
+                $this->assertSame([
+                    'api-key' => ['api-key'],
+                    'content-length' => ['95'],
+                    'Host' => ['api.rokka.io'],
+                ], $request->getHeaders());
+
+                $this->assertSame('POST', $request->getMethod());
+                $this->assertSame('https://api.rokka.io/sourceimages/foobar-organization', (string) $request->getUri());
+                $this->assertArrayHasKey('filedata', $request->getUploadedFiles());
+
+                /** @var \Zend\Diactoros\UploadedFile $upload */
+                $upload = $request->getUploadedFiles()['filedata'];
+
+                $this->assertSame('pixel.png', $upload->getClientFilename());
+                $this->assertSame('image/png', $upload->getClientMediaType());
+                $this->assertSame(95, $upload->getSize());
+
+                return true;
+            },
+            new Psr7Response(),
+            (new Response())->setProtocolVersion('1.1'),
+        ];
+    }
+
+    private function createRequest(string $uri, string $originalPath, string $organization, string $method, array $headers = [], string $uploadKey = null): Request
+    {
+        $request = Request::create($uri, $method);
+        $request->headers->add($headers);
+
+        if ($uploadKey) {
+            $request->files->set($uploadKey, new UploadedFile(__DIR__.'/../Fixtures/pixel.png', 'pixel.png', 'image/png', null, true));
+        }
+
+        $request->attributes->set('_controller', RokkaController::class);
+        $request->attributes->set('_route_params', [
+            '_rokka_original_path' => $originalPath,
+            'organization' => $organization,
+        ]);
+
+        return $request;
+    }
+}
